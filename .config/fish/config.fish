6402f53fd680b031de28b5a4a5f78010e16f8a87
kouta <fuse.kanrinin@gmail.com>
2018-10-15 23:34:22 +0900

    fix

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -19,6 +19,21 @@ alias fs-reload='fish_update_completions'
 #rbenv
 status --is-interactive; and . (rbenv init -|psub)
 
+# 一覧の上を最新として、境目をボーダーに
+set -x FZF_DEFAULT_OPTS "--reverse --border"
+
+# ディレクトリ検索で選択した項目の配下の構成をプレビューに表示する
+set -x FZF_ALT_C_OPTS   "--preview 'tree -C {} | head -200'"
+
+# ファイル検索で選択した項目の中身をプレビューに表示する
+set -x FZF_CTRL_T_OPTS  "--preview 'head -100 {}'"
+
+# エラーコードは番号で表示する
+set -g theme_show_exit_status yes
+
+# Gitのahead情報を細かく表示する
+set -g theme_display_git_ahead_verbose yes
+
 #tmux呼び出し
 init_tmux
 
