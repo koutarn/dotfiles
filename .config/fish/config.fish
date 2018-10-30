a8f715efa6f7351303375092e6a9ee4ce8a498bd
koutakun <fuse.kanrinin@gmail.com>
2018-10-31 02:09:11 +0900

    fix config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,20 @@
+#fundle
+fundle plugin fisherman/fzf
+fundle plugin fisherman/z
+fundle plugin oh-my-fish/theme-bobthefish
+fundle plugin oh-my-fish/plugin-expand
+fundle plugin fisherman/spin
+fundle plugin 0rax/fish-bd
+fundle plugin edc/bass
+fundle plugin laughedelic/pisces
+fundle plugin oh-my-fish/plugin-balias
+fundle plugin masa0x80/replace_multiple_dots.fish
+fundle plugin ryotako/fish-expand-range
+fundle plugin hagiyat/anyfff
+fundle plugin oh-my-fish/theme-bobthefish
+
+fundle init
+
 #fishへようこそを消す
 set fish_greetinm
 
@@ -64,21 +81,3 @@ if [ $SHLVL = 1 ]
   figlet hello world! 2>/dev/null
 end
 
-#fundle
-fundle plugin 'fisherman/fzf'
-
-fundle plugin fisherman/fzf
-fundle plugin fisherman/z
-fundle plugin oh-my-fish/theme-bobthefish
-fundle plugin oh-my-fish/plugin-expand
-fundle plugin fisherman/spin
-fundle plugin 0rax/fish-bd
-fundle plugin edc/bass
-fundle plugin laughedelic/pisces
-fundle plugin oh-my-fish/plugin-balias
-fundle plugin masa0x80/replace_multiple_dots.fish
-fundle plugin ryotako/fish-expand-range
-fundle plugin hagiyat/anyfff
-fundle plugin oh-my-fish/theme-bobthefish
-
-fundle init
