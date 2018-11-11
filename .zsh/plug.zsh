e7c7729c5a015346e84953dcda1fe699c1820d5a
koutakun <fuse.kanrinin@gmail.com>
2018-11-11 20:23:12 +0900

    fix

--- .zsh/plug.zsh
+++ .zsh/plug.zsh
@@ -4,13 +4,17 @@ bindkey -e
 source ~/.zplug/init.zsh
 zplug 'zsh-users/zsh-autosuggestions'                                                   #fishっぽい表示をしてくれる
 zplug "zsh-users/zsh-syntax-highlighting", defer:2                                      #syntax highlight
-zplug "b4b4r07/enhancd", use:init.sh                                                    #cdを強化
 zplug "denysdovhan/spaceship-prompt", use:spaceship.zsh, from:github, as:theme          #prompt
 zplug "zsh-users/zsh-completions"                                                       #補完ファイル
 zplug "plugins/git",   from:oh-my-zsh                                                   #git
 zplug "peterhurford/git-aliases.zsh"                                                    #git用alias
 zplug "mrowa44/emojify", as:command                                                     #コマンドライン上の文字リテラルの絵文字を emoji 化するフィルタコマンド。
 
+zplug "b4b4r07/enhancd", use:init.sh                                                    #cdを強化
+if zplug check "b4b4r07/enhancd"; then
+    export ENHANCD_FILTER="fzf --height 50% --reverse --ansi"
+    export ENHANCD_DOT_SHOW_FULLPATH=1
+fi
 
 
 if ! zplug check --verbose; then
