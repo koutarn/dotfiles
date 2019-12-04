6b35551c75d9a6db1716decb1098fa9cb6506b63
koutakun <fuse.kanrinin@gmail.com>
2019-12-05 00:42:33 +0900

    add autocomplate bind

--- .zsh/keybind.zsh
+++ .zsh/keybind.zsh
@@ -4,3 +4,4 @@ bindkey -M menuselect 'h' vi-backward-char
 bindkey -M menuselect 'j' vi-down-line-or-history
 bindkey -M menuselect 'k' vi-up-line-or-history
 bindkey -M menuselect 'l' vi-forward-char
+bindkey '^ ' autosuggest-accept
