7fd4f7f7f9b349bf8e2a8e42ef6befaceca17df9
koutarn <fuse.kanrinin@gmail.com>
2022-07-18 16:51:21 +0900

    update

--- .config/nvim/lua/plugins/flutter-tools.lua
+++ .config/nvim/lua/plugins/flutter-tools.lua
@@ -1,3 +1,23 @@
+-- ====================================================================================================
+-- flutter開発用プラグイン、内部的にdartlsを使用しているのでnvim-lspのほうでインストールしないこと
+-- ====================================================================================================
+
+-- language serverがバッファにアタッチされたときに実行する関数
+local custom_attach = function(client, bufnr)
+    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+
+    -- map用オプション
+    local opts = { noremap=true, silent=true }
+
+    -- `vim.lsp.*`関数のマッピング（多いので省略）
+    buf_set_keymap('n', 'z[', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    buf_set_keymap('n', 'z]', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+    buf_set_keymap('n', 'zh', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    buf_set_keymap('n', 'z-', '<cmd>lua vim.lsp.buf.document_symbol()<CR>', opts)
+    buf_set_keymap('n', 'z=', '<cmd>lua vim.lsp.buf.workspace_symbol()<CR>', opts)
+end
+
+
 -- alternatively you can override the default configs
 require("flutter-tools").setup {
   ui = {
@@ -11,9 +31,11 @@ require("flutter-tools").setup {
   },
 
   -- flutter_path = "~/flutter", -- <-- this takes priority over the lookup
-  flutter_lookup_cmd = nil, -- example "dirname $(which flutter)" or "asdf where flutter"
-  fvm = false, -- takes priority over path, uses <workspace>/.fvm/flutter_sdk if enabled
-  widget_guides = {enabled = true},
+  -- flutter_lookup_cmd = nil, -- example "dirname $(which flutter)" or "asdf where flutter"
+  -- fvm = false, -- takes priority over path, uses <workspace>/.fvm/flutter_sdk if enabled
+  widget_guides = {
+      enabled = true
+  },
   closing_tags = {
     prefix = "// ", -- character to use for close tag e.g. > Widget
     enabled = true -- set to false to disable
@@ -38,7 +60,7 @@ require("flutter-tools").setup {
       virtual_text = true, -- show the highlight using virtual text
       virtual_text_str = "■", -- the virtual text character to highlight
     },
-    -- on_attach = my_custom_on_attach,
+    on_attach = custom_attach,
     -- capabilities = my_custom_capabilities -- e.g. lsp_status capabilities
     --- OR you can specify a function to deactivate or change or control how the config is created
     capabilities = function(config)
