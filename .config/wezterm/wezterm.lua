9fa0dee191313f791a9eaf24946a5757a609f1ab
koutarn <fuse.kanrinin@gmail.com>
2022-08-19 02:10:58 +0900

    update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -74,18 +74,18 @@ local config = {
         bottom = 0,
     },
     window_background_opacity = 0.8,
-    window_background_image = '~/img/cyberpunk.jpg',
-    window_background_image_hsb = {
-        -- Darken the background image by reducing it to 1/3rd
-        brightness = 0.3,
+    -- window_background_image = 'C:/Users/fusek/img/cyberpunk.jpg',
+    -- window_background_image_hsb = {
+    --     -- Darken the background image by reducing it to 1/3rd
+    --     brightness = 0.3,
 
-        -- You can adjust the hue by scaling its value.
-        -- a multiplier of 1.0 leaves the value unchanged.
-        hue = 1.0,
+    --     -- You can adjust the hue by scaling its value.
+    --     -- a multiplier of 1.0 leaves the value unchanged.
+    --     hue = 1.0,
 
-        -- You can adjust the saturation also.
-        saturation = 1.0,
-    },
+    --     -- You can adjust the saturation also.
+    --     saturation = 1.0,
+    -- },
 
 
     -- 文字選択
